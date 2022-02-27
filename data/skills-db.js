91454a40d70825926bfd1aa8fb6cf8ccd3fc5ae3
joewcode <jweathingtonsr@gmail.com>
2022-02-27 00:50:11 -0600

    created skills database and connected the skills DB to the view

---
+++ data/skills-db.js
@@ -0,0 +1,23 @@
+const skills = [
+  { text: "Debugging", enjoy: false, _id: 123771 },
+  { text: "Teamwork", enjoy: true, _id: 134883 },
+  { text: "Writing Code", enjoy: true, _id: 188234 },
+];
+
+const find = (conditions, callback) => {
+  // see if this works, if not, execute the code in the catch block
+  try {
+    // make sure that conditions is an object - if not throw a TypeError
+    if (!(conditions instanceof Object)) {
+      throw new TypeError("Please pass in an object");
+    }
+    // If the object is empty, return all the todos
+    if (Object.keys(conditions).length === 0) return callback(null, skills);
+    // deal with errors
+  } catch (error) {
+    console.log(error);
+    callback(error, []);
+  }
+};
+
+export { find };
---
+++ routes/skills.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import * as skillDb from "../data/skills-db.js";
+const router = Router();
+
+/* GET skills listing. */
+router.get("/", function (req, res) {
+  skillDb.find({}, function (error, skills) {
+    res.render("skills/index", {
+      skills: skills,
+      error: error,
+    });
+  });
+});
+
+export { router };
